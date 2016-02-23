ef1f4ef82a630a4efa922316ff8998a19b77822d
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2016-02-23 11:34:12 -0500

    dj - updating script to pull new code and put where it belongs

--- add_plist_entry.sh
+++ add_plist_entry.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -le 
 
 echo "adding boxen.update.plist to launchctl... possibly removed by ADS push to machine."
+cd /tmp
+rm -rf /tmp/workstation-files
+git clone http://admin@stash/scm/cypher/workstation-files.git
+
+cp /tmp/workstation-files/LaunchAgents/boxen.update.plist ~/Library/LaunchAgents/boxen.update.plist
+
 launchctl unload ~/Library/LaunchAgents/boxen.update.plist
 launchctl load ~/Library/LaunchAgents/boxen.update.plist
