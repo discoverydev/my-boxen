f018e2973210c9aeebe511c747496a34111e18f9
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2016-03-14 10:32:56 -0400

    Coleman - edited add plist script to use absolute paths everywhere.

--- add_plist_entry.sh
+++ add_plist_entry.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -le 
 
 echo "adding boxen.update.plist to launchctl... possibly removed by ADS push to machine."
-cd /tmp
 rm -rf /tmp/workstation-files
-git clone http://admin@stash/scm/cypher/workstation-files.git
+git clone http://admin@stash/scm/cypher/workstation-files.git /tmp/workstation-files
 
 cp /tmp/workstation-files/LaunchAgents/boxen.update.plist ~/Library/LaunchAgents/boxen.update.plist
 
-launchctl unload ~/Library/LaunchAgents/boxen.update.plist
+#launchctl unload ~/Library/LaunchAgents/boxen.update.plist
 launchctl load ~/Library/LaunchAgents/boxen.update.plist
 launchctl list | grep boxen
