f945ea1403b8231360fa6369ecd2390262bf0d6b
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2016-04-04 15:04:54 -0400

    Coleman - this might have worked, committing so I can try on Warlock

--- manifests/site.pp
+++ manifests/site.pp
@@ -77,10 +77,32 @@ node default {
 
   nodejs::version { 'v0.12.2': }
   class { 'nodejs::global': version => 'v0.12.2' }
-  nodejs::module { 'npm': node_version => 'v0.12.2' }
-  nodejs::module { 'appium@1.4.13': node_version => 'v0.12.2' }
-  nodejs::module { 'ios-sim': node_version => 'v0.12.2' }
-  nodejs::module { 'phantomjs': node_version => 'v0.12.2' }
+
+  npm_module { "npm": 
+    module       => 'npm',
+    node_version => $version
+  }
+
+  npm_module { 'appium':
+    module       => 'appium@1.4.13',
+    version      => '~> 1.4.13',
+    node_version => $version
+  }
+
+  npm_module { 'ios-sim':
+    module       => 'ios-sim',
+    node_version => $version
+  }
+
+  npm_module { 'phantomjs': 
+    module       => 'phantomjs',
+    node_version => $version
+  }
+
+  #nodejs::module { 'npm': node_version => 'v0.12.2' }
+  #nodejs::module { 'appium@1.4.13': node_version => 'v0.12.2' }
+  #nodejs::module { 'ios-sim': node_version => 'v0.12.2' }
+  #nodejs::module { 'phantomjs': node_version => 'v0.12.2' }
 
   #
   # RUBY stuff
