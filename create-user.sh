8fce71adcd695d55ff853b334ea628394d0bcc29
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-05-05 12:51:35 -0400

    coleman - fixed syntax err

--- create-user.sh
+++ create-user.sh
@@ -5,8 +5,8 @@ HOST=$2
 
 dscl . create /Users/minion${NUMBER} 
 dscl . create /Users/minion${NUMBER} UserShell /bin/bash
-dscl . create /Users/minion${NUMBER} RealName "${HOST} Minion ${NUMBER}
-dscl . create /Users/minion${NUMBER} UniqueID "100${NUMBER}" 
+dscl . create /Users/minion${NUMBER} RealName "${HOST} Minion ${NUMBER}"
+dscl . create /Users/minion${NUMBER} UniqueID "100${NUMBER}"
 dscl . create /Users/minion${NUMBER} PrimaryGroupID 20
 dscl . create /Users/minion${NUMBER} NFSHomeDirectory /Users/minion${NUMBER}
 dscl . passwd /Users/minion${NUMBER} M0bileL0yalty
