03a940f9b63fe90e6e4e65773ee2773535c6c7b6
Discovery Dev <discoverydev.nightcrawler@gmail.com>
2016-06-22 14:07:08 -0400

    Coleman - deprecate run_before

--- run_before_boxen.sh
+++ run_before_boxen.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+echo "This script is deprecated. Please see the README or Coleman for more information"
+
 #USER=ga-mlsdiscovery
-USER=ga-mlsdiscovery
-SERVER=192.168.8.36
-
-source ~/.profile
-
-echo "* adding $SERVER to known_hosts"
-ssh-keyscan $SERVER > ~/.ssh/known_hosts
-
-TARFILE=boxen.tar
-DEST=/opt
-SRC=/opt
-
-echo "* copy $TARFILE from $SERVER ($SRC) to $DEST"
-mkdir -p $DEST
-
-pushd $DEST
-echo "  user $USER"
-rsync -ru --progress $USER@$SERVER:$SRC/$TARFILE $DEST/$TARFILE
-tar xkvf $TARFILE
-popd
+#USER=ga-mlsdiscovery
+#SERVER=192.168.8.36
+#
+#source ~/.profile
+#
+#echo "* adding $SERVER to known_hosts"
+#ssh-keyscan $SERVER > ~/.ssh/known_hosts
+#
+#TARFILE=boxen.tar
+#DEST=/opt
+#SRC=/opt
+#
+#echo "* copy $TARFILE from $SERVER ($SRC) to $DEST"
+#mkdir -p $DEST
+#
+#pushd $DEST
+#echo "  user $USER"
+#rsync -ru --progress $USER@$SERVER:$SRC/$TARFILE $DEST/$TARFILE
+#tar xkvf $TARFILE
+#popd
