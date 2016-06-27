592d96c367f4c293df532950c976255c12813ae5
Discovery Dev <discoverydev.bishop@gmail.com>
2016-06-27 17:04:41 -0400

    Coleman - this line didn't do anything...

--- set_stash_password.sh
+++ set_stash_password.sh
@@ -5,7 +5,6 @@ read -s PW
 echo ""
 
 : ${PW:?"Need to set password"}
-git-credential-osxkeychain
 security add-internet-password -a $(whoami) -s stash -r http -U -T /opt/boxen/homebrew/bin/git-credential-osxkeychain -w $PW
 
 echo "Password for $(whoami) set successfully"
