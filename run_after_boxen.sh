cc27f3d3afc26ebd072c58a77447d62aecb99e0a
Discovery Dev <discoverydev.cable@gmail.com>
2016-07-20 13:11:09 -0400

    Coleman - run_after can find ids

--- run_after_boxen.sh
+++ run_after_boxen.sh
@@ -24,7 +24,12 @@ popd
 HAXM_DIR="/opt/android-sdk/extras/intel/Hardware_Accelerated_Execution_Manager"
 sudo ${HAXM_DIR}/silent_install.sh
 
-echo 'y' | android update sdk -a -u -t 81
-echo 'y' | android update sdk -a -u -t 153
+get_android_pkg(){
+    # gets the id number of the pkg - necessary because they change
+    android list sdk --all -e | grep $1 | cut -d ' ' -f 2
+}
+
+echo 'y' | android update sdk -a -u -t $(get_android_pkg 'sys-img-x86-android-21')
+echo 'y' | android update sdk -a -u -t $(get_android_pkg 'extra-intel-Hardware_Accelerated_Execution_Manager')
 
 echo 'no' | android create avd --name Nexus_5_API_21_Test_Device -t android-21 -b default/x86 -f  
