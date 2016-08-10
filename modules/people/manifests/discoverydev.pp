4ab769ce1ee0277bb5aba93fa399d519c2a9072b
Discovery Dev <discoverydev.stryker@gmail.com>
2016-08-10 17:26:41 -0400

    Coleman - commented out Sonar runner - we're not using it, and it's broken

--- modules/people/manifests/discoverydev.pp
+++ modules/people/manifests/discoverydev.pp
@@ -95,10 +95,10 @@ class people::discoverydev {
     command => 'sudo pfctl -f /etc/pf.conf'
   }
 
-  file { "sonar-runner.properties":
-    name => "${homebrew::config::installdir}/Cellar/sonar-runner/2.5/libexec/conf/sonar-runner.properties",
-    source => "${boxen::config::repodir}/manifests/files/sonar-runner.properties",
-    require => Package['sonar-runner'],
-  }
+  #file { "sonar-runner.properties":
+  #  name => "${homebrew::config::installdir}/Cellar/sonar-runner/2.5/libexec/conf/sonar-runner.properties",
+  #  source => "${boxen::config::repodir}/manifests/files/sonar-runner.properties",
+  #  require => Package['sonar-runner'],
+  #}
 
 }
