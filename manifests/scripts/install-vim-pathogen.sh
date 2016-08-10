9db8c67d399697891f2740e49e80427220491d4b
Discovery Dev <discoverydev.cable@gmail.com>
2016-08-10 15:18:30 -0400

    coleman - vim pathogen, placeholder commit. tabling this for now, the script isn't working as expected and I don't want to spend too much time on it right now

---
+++ manifests/scripts/install-vim-pathogen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script installs Vim pathogen, a convenient package manager for Vim. 
+# Please see https://github.com/tpope/vim-pathogen for more information.
+
+if [ ! -f $HOME/.vim/autolaod/pathogen.vim ]; then
+    echo "Installing Vim Pathogen..."
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+else
+    echo "Pathogen already present, exiting without changes."
+fi
