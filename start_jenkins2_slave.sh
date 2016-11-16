62fa6856950c4c3f9aa100178191cfdf9f2f5e13
Discovery Dev <discoverydev.wolverine@gmail.com>
2016-11-16 10:47:10 -0500

    Coleman - jenkins2 start script now attempts to start an android and an ios node

--- start_jenkins2_slave.sh
+++ start_jenkins2_slave.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 javaws http://jenkins2/computer/`hostname -s`/slave-agent.jnlp
+javaws http://jenkins2/computer/`hostname -s`_ANDROID/slave-agent.jnlp
+javaws http://jenkins2/computer/`hostname -s`_IOS/slave-agent.jnlp
