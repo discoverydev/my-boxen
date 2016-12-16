2c266dac7e0ff00b8521ada3e4f8aa4b4f05e01e
Discovery Dev <discoverydev.juggernaut@gmail.com>
2016-12-16 14:24:24 -0500

    Coleman, Righter - update android_get_sdk to be smarter

--- android_get_sdk.sh
+++ android_get_sdk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 
 USER=ga-mlsdiscovery
 SERVER=192.168.8.36 # mystique
@@ -8,9 +8,11 @@ source ~/.profile
 echo "* adding ${SERVER} to known_hosts"
 ssh-keyscan ${SERVER} > ~/.ssh/known_hosts
 
-TARFILE=tailored_backup.tar.gz
+ANDROID_SDK=android-sdk
+TARFILE=${ANDROID_SDK}.tar.gz
 DESTINATION=/opt
 SRC=/Users/${USER}/tailored_backup
+ANDROID_SDK=android-sdk
 
 echo "* copy ${TARFILE} from ${SERVER} (${SRC}) to ${DESTINATION}"
 
@@ -18,10 +20,19 @@ mkdir -p ${DESTINATION}
 
 pushd ${DESTINATION}
 echo "  user ${USER}"
-rsync -ru --progress ${USER}@${SERVER}:${SRC}/${TARFILE} ${DESTINATION}/${TARFILE}
-tar xzkvf ${TARFILE}
+if [[ -e ${DESTINATION}/${ANDROID_SDK} ]]; then
+    echo "Android SDK detected - updating from gold image"
+    echo "remove /opt/android-sdk to force a full redownload of the SDK."
+    rsync -au --progress ${USER}@${SERVER}:${SRC}/${ANDROID_SDK} ${DESTINATION}/${ANDROID_SDK}
+else 
+    echo "No Android SDK detected - cloning from gold image"
+    echo "rsync --progress ${USER}@${SERVER}:${SRC}/${TARFILE} ${DESTINATION}/${TARFILE} "
+    rsync --progress ${USER}@${SERVER}:${SRC}/${TARFILE} ${DESTINATION}/${TARFILE} 
+
+    tar xzkvf ${TARFILE}
+fi
+
 popd
 
 HAXM_DIR="/opt/android-sdk/extras/intel/Hardware_Accelerated_Execution_Manager"
 sudo ${HAXM_DIR}/silent_install.sh
-
