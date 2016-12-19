c67b9cea95d045857a385354d434c30211dde3ba
Discovery Dev <discoverydev.juggernaut@gmail.com>
2016-12-19 11:12:12 -0500

    Coleman, Righter - add_plist_entry now adds a cleanup script

--- add_plist_entry.sh
+++ add_plist_entry.sh
@@ -1,11 +1,21 @@
-#!/bin/bash -le 
+#!/bin/bash -le
+
+reload() {
+  local agent_path=~/Library/LaunchAgents/${1}
+  launchctl unload $agent_path
+  launchctl load $agent_path
+}
 
 echo "adding boxen.update.plist to launchctl... possibly removed by ADS push to machine."
 rm -rf /tmp/workstation-files
 git clone http://admin@stash/scm/cypher/workstation-files.git /tmp/workstation-files
 
 cp /tmp/workstation-files/LaunchAgents/boxen.update.plist ~/Library/LaunchAgents/boxen.update.plist
+cp /tmp/workstation-files/LaunchAgents/cleanup.mac.plist ~/Library/LaunchAgents/cleanup.mac.plist
 
-launchctl unload ~/Library/LaunchAgents/boxen.update.plist
-launchctl load ~/Library/LaunchAgents/boxen.update.plist
+
+reload 'boxen.update.plist'
+reload 'cleanup.mac.plist'
+
 launchctl list | grep boxen
+launchctl list | grep cleanup
