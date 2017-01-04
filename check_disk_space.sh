615d380bfd9845b2d8ead6c5452d39498a23a91f
Discovery Dev <discoverydev.juggernaut@gmail.com>
2017-01-04 13:13:22 -0500

    Coleman - add some output to check_disk_space for when it is run manually

--- check_disk_space.sh
+++ check_disk_space.sh
@@ -1,5 +1,10 @@
 #!/bin/bash 
 
-PERCENTAGE=$(df -H | awk -v HOST=$(hostname) '/\/dev\/disk.*/ { print substr($5, 0, length($5)-1) }')
+HOST=$(hostname -s)
 
-curl -X POST http://jenkins2/job/DiskUsage_$(hostname -s)/buildWithParameters?PERCENTAGE=${PERCENTAGE}
+PERCENTAGE=$(df -H | awk -v HOST=${HOST} '/\/dev\/disk.*/ { print substr($5, 0, length($5)-1) }')
+
+echo "Disk usage: ${PERCENTAGE}%"
+echo "Sending disk usage check results for ${HOST} to Jenkins."
+
+curl -X POST http://jenkins2/job/DiskUsage_${HOST}/buildWithParameters?PERCENTAGE=${PERCENTAGE}
