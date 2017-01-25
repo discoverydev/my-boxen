df2c2b1063e0d421aaaadbb31953858acefa3f92
Discovery Dev <discoverydev.havok@gmail.com>
2017-01-25 09:47:15 -0500

    added Guy's favorite git aliases

--- modules/people/manifests/discoverydev/config/gitconfig.pp
+++ modules/people/manifests/discoverydev/config/gitconfig.pp
@@ -7,9 +7,12 @@ class people::discoverydev::config::gitconfig ( $email = downcase($hostname) ) {
 
   git::config::global {
     'user.name'                  : value => 'Discovery Dev';
-    'user.email'                 : value => "discoverydev.${email}@gmail.com"; 
+    'user.email'                 : value => "discoverydev.${email}@gmail.com";
     'push.default'               : value => 'simple';
     'alias.nuke'                 : value => 'reset --hard HEAD';
+    'alias.co'                   : value => 'checkout';
+    'alias.br'                   : value => 'branch';
+    'alias.hist'                 : value => "log --pretty=format:'%h %ai | %s%d [%an]' --graph --date=short";
     'status.showUntrackedFiles'  : value => 'all';
   }
 
