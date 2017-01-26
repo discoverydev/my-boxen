037adb9ba0c1b42b3322d20a8d0f6ababc90563c
Discovery Dev <discoverydev.doop@gmail.com>
2017-01-26 09:11:38 -0500

    added jenkins2 slave plist

--- add_plist_entry.sh
+++ add_plist_entry.sh
@@ -6,7 +6,7 @@ reload() {
   launchctl load $agent_path
 }
 
-place_plist() { 
+place_plist() {
     cp /tmp/workstation-files/LaunchAgents/${1} ~/Library/LaunchAgents/${1}
 }
 
@@ -18,10 +18,13 @@ git clone http://admin@stash/scm/cypher/workstation-files.git /tmp/workstation-f
 place_plist 'boxen.update.plist'
 place_plist 'cleanup.mac.plist'
 place_plist 'disk.usage.plist'
+place_plist 'jenkins2.node.plist'
 
 reload 'boxen.update.plist'
 reload 'cleanup.mac.plist'
 reload 'disk.usage.plist'
+reload 'jenkins2.node.plist'
 
 launchctl list | grep boxen
 launchctl list | grep cleanup
+launchctl list | grep jenkins2
