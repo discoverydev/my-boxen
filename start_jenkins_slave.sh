beb885ffeed11991f6b8baa55456c78d140a8bb8
ga-mlsdiscovery <ga-mlsdiscovery@gmail.com>
2017-02-01 14:34:53 -0500

    Coleman - remove old start_slave script

--- start_jenkins_slave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "* starting jenkins slave"
-javaws http://jenkins/computer/`hostname -s`/slave-agent.jnlp
-echo "* starting jenkins IOS slave"
-javaws http://jenkins/computer/`hostname -s`_IOS/slave-agent.jnlp
-echo "* starting jenkins ANDROID slave"
-javaws http://jenkins/computer/`hostname -s`_ANDROID/slave-agent.jnlp
