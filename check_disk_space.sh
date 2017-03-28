e059e545951ed8a6e753b241756a2d2ba22067bc
Discovery Dev <discoverydev.voltron2@gmail.com>
2017-03-28 11:36:16 -0400

    Coleman, Sham - update regex to ensure we only get the primary drive's usage percentage

--- check_disk_space.sh
+++ check_disk_space.sh
@@ -1,8 +1,8 @@
-#!/bin/bash 
+#!/bin/bash -x
 
 HOST=$(hostname -s)
 
-PERCENTAGE=$(df -H | awk -v HOST=${HOST} '/\/dev\/disk.*/ { print substr($5, 0, length($5)-1) }')
+PERCENTAGE=$(df -H | awk -v HOST=${HOST} '/\/dev\/disk.*\/$/ { print substr($5, 0, length($5)-1) }')
 
 echo "Disk usage: ${PERCENTAGE}%"
 echo "Sending disk usage check results for ${HOST} to Jenkins."
