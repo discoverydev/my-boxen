af6658b598579a07712ae3047eb22c6846b5e76a
Discovery Dev <discoverydev.colossus@gmail.com>
2017-03-30 13:41:53 -0400

    coleman - fix error in solarized theme install script

--- manifests/scripts/install-vim-colors-solarized.sh
+++ manifests/scripts/install-vim-colors-solarized.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-git clone git://github.com/altercation/vim-colors-solarized.git /Users/ga-mlsdiscovery/.vim/bundle/vim-colors-solarized
+if [[ ! -e ~/.vim/bundle/vim-colors-solarized ]]; then
+    git clone git://github.com/altercation/vim-colors-solarized.git /Users/ga-mlsdiscovery/.vim/bundle/vim-colors-solarized
+fi
